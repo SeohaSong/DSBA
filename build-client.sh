589391045a3b6ea77f7c0e34f007027bb8935a3e
seohasong <tisutoo@gmail.com>
2018-08-14 18:12:06 +0900

    update origin master

--- build-client.sh
+++ build-client.sh
@@ -1,8 +1,10 @@
+(
+    prod_repo_path=~/SEOHASONG/gitfree/seohasong.github.io
 
-prod_repo_path='~/SEOHSASONG/gitfree/seohasong.github.io'
-
-cd DSBA.client\
-&& npm run build:prerender\
-&& mv ./dist/DSBA $prod_repo_path
-&& cd $prod_repo_path
-&& (git add .; git commit -m 'update origin master'; git push origin master)
+    cd ./DSBA.client\
+    && npm run build:prerender\
+    && rm -rf $prod_repo_path'/DSBA'\
+    && mv ./dist/DSBA $prod_repo_path\
+    && cd $prod_repo_path\
+    && (git add .; git commit -m 'update origin master'; git push origin master)
+)
