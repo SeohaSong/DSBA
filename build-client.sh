1a2faf6538dad021b3f98be6377864ae091004ac
seohasong <tisutoo@gmail.com>
2018-08-23 17:39:22 +0900

    update origin master

--- build-client.sh
+++ build-client.sh
@@ -1,10 +1,20 @@
 (
-    prod_repo_path=~/SEOHASONG/seohasong.github.io
+
+    if [[ "$PATH" =~ /mnt/c/Windows/System32 ]]
+    then
+        prod_repo_path="/mnt/c/Users/"$(whoami)/SEOHASONG/seohasong.github.io
+    else
+        prod_repo_path=~/SEOHASONG/seohasong.github.io
+    fi
 
     cd ./client\
     && npm run build:prerender\
     && rm -rf $prod_repo_path'/DSBA'\
     && mv ./dist/DSBA $prod_repo_path\
     && cd $prod_repo_path\
-    && (git add .; git commit -m 'update origin master'; git push origin master)
+    && (
+        git add .
+        git commit -m 'update origin master'
+        git push origin master
+    )
 )
