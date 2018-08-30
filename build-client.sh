16f900ee538783172bb19f541aae0f8553a68edf
seohasong <tisutoo@gmail.com>
2018-08-30 16:00:42 +0900

    update origin master

--- build-client.sh
+++ build-client.sh
@@ -1,16 +1,28 @@
 (
 
+    if [ "$1" = "seohasong" ]
+    then
+        prod_repo_path_part='/SEOHASONG/seohasong.github.io/DSBA'
+        npm_cmd='build:prerender-seohasong'
+    elif [ "$1" = "" ]
+    then
+        prod_repo_path_part='/SEOHASONG/gitfree/dsba-koreauniv.github.io'
+        npm_cmd='build:prerender'
+    else
+        exit
+    fi
+
     if [[ "$PATH" =~ /mnt/c/Windows/System32 ]]
     then
-        prod_repo_path="/mnt/c/Users/"$(whoami)/SEOHASONG/seohasong.github.io
+        prod_repo_path="/mnt/c/Users/"$(whoami)$prod_repo_path_part
     else
-        prod_repo_path=~/SEOHASONG/seohasong.github.io
+        prod_repo_path=~$prod_repo_path_part
     fi
 
     cd ./client\
-    && npm run build:prerender\
-    && rm -rf $prod_repo_path'/DSBA'\
-    && mv ./dist/DSBA $prod_repo_path\
+    && npm run $npm_cmd\
+    && rm -rf $prod_repo_path'/*'\
+    && mv ./dist/DSBA/* $prod_repo_path\
     && cd $prod_repo_path\
     && (
         git add .
