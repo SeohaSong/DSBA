668f17038b5a51c22ec5214064eea60e22e0a2ce
seohasong <tisutoo@gmail.com>
2018-08-30 16:26:39 +0900

    update origin master

--- build-client.sh
+++ build-client.sh
@@ -2,11 +2,11 @@
 
     if [ "$1" = "seohasong" ]
     then
-        prod_repo_path_part='/SEOHASONG/seohasong.github.io/DSBA'
+        repo_path_part='/SEOHASONG/seohasong.github.io/DSBA'
         npm_cmd='build:prerender-seohasong'
     elif [ "$1" = "" ]
     then
-        prod_repo_path_part='/SEOHASONG/gitfree/dsba-koreauniv.github.io'
+        repo_path_part='/SEOHASONG/gitfree/dsba-koreauniv.github.io'
         npm_cmd='build:prerender'
     else
         exit
@@ -14,19 +14,18 @@
 
     if [[ "$PATH" =~ /mnt/c/Windows/System32 ]]
     then
-        prod_repo_path="/mnt/c/Users/"$(whoami)$prod_repo_path_part
+        repo_path="/mnt/c/Users/"$(whoami)$repo_path_part
     else
-        prod_repo_path=~$prod_repo_path_part
+        repo_path=~$repo_path_part
     fi
 
     cd ./client\
     && npm run $npm_cmd\
-    && rm -rf $prod_repo_path'/*'\
-    && mv ./dist/DSBA/* $prod_repo_path\
-    && cd $prod_repo_path\
-    && (
-        git add .
-        git commit -m 'update origin master'
-        git push origin master
-    )
+    && rm -rf $repo_path/*\
+    && mv ./dist/DSBA/* $repo_path\
+    && rm -rf ./dist\
+    && cd $repo_path
+    git add .
+    git commit -m 'update origin master'
+    git push origin master
 )
