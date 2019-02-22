859f34bb6a58b3932051d249665a568209c49929
seohasong <tisutoo@gmail.com>
2019-02-22 16:17:21 +0900

    continue previous work

--- build-client.sh
+++ build-client.sh
@@ -1,24 +1,19 @@
 (
-    if [ "$1" = "" ]
-    then
-        dir_path=$HOME_PATH/SEOHASONG/seohasong.github.io/DSBA
-        npm_cmd='build:prerender-seohasong'
-    elif [ "$1" = "production" ]
-    then
-        dir_path=$( cd $( dirname $BASH_SOURCE ) && pwd )/dsba-koreauniv.github.io
-        npm_cmd='build:prerender'
-    else
-        exit
-    fi
-
-    cname=dsba.korea.ac.kr \
-    && cd ./client \
-    && npm run $npm_cmd \
-    && rm -r $dir_path/* \
-    && mv ./dist/client/* $dir_path \
-    && rm -r ./dist \
-    && cd $dir_path \
-    && git checkout master \
-    && echo $cname > CNAME \
-    && gitgit
+    trap 'return 0' ERR
+    dir_path=$( cd $( dirname $BASH_SOURCE ) && pwd )/dsba-koreauniv.github.io
+    cd ./client
+    npm run build:prerender
+    [ -d $dir_path ] && {
+        rm -r $dir_path/*
+        mv ./dist/client/* $dir_path
+        echo dsba.korea.ac.kr > $dir_path/CNAME
+    }
+    rm -r ./dist
+    (
+        cd $dir_path
+        git checkout master
+        gitgit
+    )
 )
+
+# "build:client-and-server-bundles-seohasong": "ng build --base-href /DSBA/ --prod && ng run client:server"
